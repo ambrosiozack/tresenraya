a9e0c63d053a4c44ee40927faa4192c8d5c03126
Oscar Lopez Garcia <ambrosiozackweb@gmail.com>
2022-11-03 02:15:45 +0100

    Crear las funciones de jugada y comprobar si ha ganado

---
+++ tresenraya/Clases/Jugador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tresenraya.Clases
+{
+    internal class Jugador
+    {
+        internal string Nombre { get; set; }
+        internal string Ficha { get; set; }
+    }
+}
